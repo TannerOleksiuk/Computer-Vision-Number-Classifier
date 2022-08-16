bbf57829e013f5f85827331451bdcf462924172c
TannerOleksiuk <tanneroleksiuk@gmail.com>
2022-08-16 13:56:25 -0700

    Tidied up classification script. Edited README to include dependencies

--- MNIST_Classify.py
+++ MNIST_Classify.py
@@ -1,52 +1,69 @@
 import tensorflow as tf
 import numpy as np
 import cv2 as cv
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt # Not required unless you want to use it for displaying images
 from time import sleep
 print("Tensorflow Version:", tf.__version__)
 
-#Load data set into train and test variables
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# Load data set into train and test variables (Not required for normal use, only testing)
+#mnist = tf.keras.datasets.mnist
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-#Normalize and convert data to floats
-x_train, x_test = x_train/255.0, x_test / 255.0
+# Normalize and convert data to floats
+#x_train, x_test = x_train/255.0, x_test / 255.0
 
+# Load model
 MNIST_model = tf.keras.models.load_model("MNIST_Seq_model.h5")
 # Generate probabillity model
 probability_model = tf.keras.Sequential([MNIST_model, tf.keras.layers.Softmax()])
 
-#classify a single input
+"""
+ Convert Single Image to 3D Numpy Array
+ Our model requires a 3D input so convert input image to tuple if not already
+ img: Image of shape (x, y) where x,y are the dimensions of the imension.
+ Therefore image must be grayscale or single colour channel.
+"""
+def convert3D_input_image(img):
+    if(img.ndim != 3):
+        prep_img = np.array([img])
+        return prep_img
+
+"""
+ Classify a single input
+ input: Image of shape (28,28) or (1,28,28)
+ Image should ideally match formatting of MNIST dataset, binarized 20x20 image padded with 4 pixels
+ on each side to make
+"""
 def classify_single(input):
+
+    # Is the input a 3d? If not convert it.
     if(input.ndim != 3):
-        input = tuplify_input_image(input)
+        input = convert3D_input_image(input)
     
     if(input.shape[1] != 28 or input.shape[2] != 28):
         print(input.shape)
         print("Invalid Image Size. Resize image to 28x28")
         return
 
-    #y = MNIST_model.predict(input, verbose=2)[0]
-    #y_prob = Prob_model.predict(input)[0]
-    #print("Prediction array: ",y)
+    #y = MNIST_model.predict(input, verbose=2)[0] # If wanted can use this instead of probabillity model
+    #print("Prediction array: ",y) # Debug print to see prediction scores
+
     y_prob = probability_model(input)[0]
     y = int(np.where(y_prob == np.amax(y_prob))[0])
+
     # Grab probabillity of that number
     y_prob = y_prob[y]
     y_prob = y_prob.numpy()
 
     #y = int(np.where(y == np.amax(y))[0]) # Not needed now that using prob model
-    #y_prob = int(np.where(y == np.amax(y))[0])
-    #print("The number is", y, y_prob)
+
+    #print("The number is", y, y_prob) # Debug print
+
     return y, y_prob
 
-#Our model requires a tuple so convert input image to tuple if not already
-def tuplify_input_image(img):
-    if(img.ndim != 3):
-        prep_img = np.array([img])
-        return prep_img
 
-# TODO: Turn this image processing into a function -> likely in another file
+# Below was used for testing. Uncomment to import a single image and use it in classify function
+
 #img = cv.imread('seven.png', cv.IMREAD_GRAYSCALE)
 #img = img/255.0
 #th,img = cv.threshold(img, 0.5, 1, cv.THRESH_BINARY_INV)
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Computer-Vision-Number-Classifier
 Small project to attempt to get some experience using tensorflow and opencv in conjunction to classify written numbers as viewed from my webcam. Using the keras sequential model and the MNIST dataset example from the TensorFlow tutorials.
 
+Requires OpenCV, TensorFlow, and Numpy. 
+
+Optionally matplotlib may be installed for testing and viewing images with its image viewer as it is better suited than the OpenCV viewer in some cases.
+
 For more information visit my website: https://www.tanneroleksiuk.ca/
